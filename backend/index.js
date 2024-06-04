8605dda7e6e4aa45a2449a4690cd8a46aaa6c4c1
ehuang0709 <ehuang0709@gmail.com>
2024-06-04 01:02:24 -0400

    Set up express.js server and connection to mongodb

--- backend/index.js
+++ backend/index.js
@@ -5,15 +5,20 @@ const mongoose = require("mongoose");
 
 mongoose.connect(config.connectionString);
 
+const User = require("./models/user.model");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
+const jwt = require("jsonwebtoken");
+const { authenticateToken } = require("./utilities");
 
 app.use(express.json());
 
 app.use(
     cors({
-        origin: "+",
+        origin: "*",
     })
 );
 
@@ -21,7 +26,47 @@ app.get("/", (req, res) => {
     res.json({ data: "hello" });
 })
 
+// create account
+app.post("/create-account", async (req, res) => {
+    const { fullName, email, password } = req.body;
 
+    if (!fullName) {
+        return res.status(400).json({ error: true, message: "Full Name is required" });
+    }
+
+    if (!email) {
+        return res.status(400).json({ error: true, message: "Email is required" });
+    }
+
+    if (!password) {
+        return res.status(400).json({ error: true, message: "Password is required" });
+    }
+
+    const isUser = await User.findOne({ email: email });
+
+    if (isUser) {
+        return res.json({ error: true, message: "User already exists!", });
+    }
+
+    const user = new User ({
+        fullName, 
+        email, 
+        password, 
+    });
+
+    await user.save();
+
+    const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "36000m",
+    });
+
+    return res.json({
+        error: false,
+        user, 
+        accessToken, 
+        message: "Registration Successful",
+    });
+});
 
 
 app.listen(8000);
