8a8fb19dbf35c03e4630d662860c4422eb1aff63
ehuang0709 <ehuang0709@gmail.com>
2024-06-13 01:20:25 -0400

    Set up process of getting user account info and sign up

--- frontend/notex/src/pages/SignUp/SignUp.jsx
+++ frontend/notex/src/pages/SignUp/SignUp.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import PasswordInput from '../../components/Input/PasswordInput'
 import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosInstance';
 
 
 const SignUp = () => {
@@ -11,6 +12,8 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
 
   const handleSignUp = async (e) => {
     e.preventDefault();
@@ -31,6 +34,33 @@ const SignUp = () => {
     }
 
     setError('')
+
+    // Sign Up API call
+    try {
+      const response = await axiosInstance.post("/create-account", {
+        fullName: name,
+        email: email,
+        password: password,
+      });
+
+      // Handle successful sign up response
+      if (response.data && response.data.error) {
+        setError(response.data.message);
+        return;
+      }
+
+      if (response.data && response.data.accessToken) {
+        localStorage.setItem("token", response.data.accessToken);
+        navigate('/dashboard')
+      }
+    } catch (error) {
+      // Handle login error
+      if (error.response && error.response.data && error.response.data.message) {
+        setError(error.response.data.message);
+      } else {
+        setError("An unexpected error has occurred. Please try again.");
+      }
+    }
   };
 
   return (
