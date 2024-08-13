c90f5bb2cef98db7b43ee5bfb18e16f69ed22de2
ehuang0709 <ehuang0709@gmail.com>
2024-08-13 13:54:00 -0400

    Reverted authentication changes

--- backend/utilities.js
+++ backend/utilities.js
@@ -4,18 +4,10 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(' ')[1];
 
-    console.log("Received token:", token);
-
-    if (token == null) {
-        console.log("No token provided");
-        return res.sendStatus(401);
-    }
+    if (!token) return res.sendStatus(401);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) {
-            console.log("Token verification failed:", err);
-            return res.sendStatus(403);
-        }
+        if (err) return res.sendStatus(401);
         req.user = user;
         next();
     });
